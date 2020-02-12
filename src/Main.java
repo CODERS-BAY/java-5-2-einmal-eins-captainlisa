13a97350b0d1070adb72ee804d8329eb3f08538e
Lisa-Maria Plakolb <lisa.plakolb96@gmx.at>
2020-02-12 10:41:58 +0100

    Format with printf

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // formatting with printf
         for (int i = 1; i <= 10; i++) {
             System.out.println(i + "er-Reihe:");
 
             for (int k = 1; k <= 10; k++) {
-                System.out.println(i + "*" + k + "=" + (i * k));
+                System.out.printf("%2d * %2d = %3d\n", i, k, (i * k));
             }
         }
+
     }
 }
 
